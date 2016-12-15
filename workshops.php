857e7244ec28d74bad9f4326fbc017195166f165
george <robopop@radarboy.com>
2016-12-15 14:37:48 +0800

    added workshops

---
+++ workshops.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+<head>
+
+	<!-- Basic Page Needs
+  ================================================== -->
+	<meta charset="utf-8">
+	<title>George Gally - Grunt Chat</title>
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<!-- Mobile Specific Metas
+  ================================================== -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<!-- CSS
+  ================================================== -->
+	<link rel="stylesheet" href="stylesheets/base.css">
+	<link rel="stylesheet" href="stylesheets/skeleton.css">
+	<link rel="stylesheet" href="stylesheets/layout.css">
+
+	<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+	<!-- Favicons
+	================================================== -->
+	<link rel="shortcut icon" href="images/favicon.ico">
+	<link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+
+</head>
+<body >
+
+	<div class="back-header">
+
+		<a href="index.html#broadcast">< Back</a>
+
+	</div>
+
+
+	<div class="container">
+
+	<div class="sixteen columns add-bottom">
+			<h2 class="remove-bottom add-top add-bottom">Workshops</h2>
+			<h4>Creative Coding. Javascript. Motion Reactive. Sound Reactive.</h4>
+
+	</div>
+
+	<div class="two-thirds column">
+
+
+		<blockquote class="twitter-tweet" data-lang="en"><p lang="en" dir="ltr">Creative coding workshop by <a href="https://twitter.com/radarboy_japan">@radarboy_japan</a> at <a href="https://twitter.com/xinchejian">@xinchejian</a>.<br>Please meet &quot;Blood Rain&quot;, my favorite:<a href="https://t.co/15kI9HRQtv">https://t.co/15kI9HRQtv</a> <a href="https://t.co/stnjqsCvOr">pic.twitter.com/stnjqsCvOr</a></p>&mdash; Drix (@CedHon) <a href="https://twitter.com/CedHon/status/802785132578885632">November 27, 2016</a></blockquote>
+		<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+
+
+</div>
+
+<div class="one-third column">
+<p>
+Since starting my #code365 project there's been a massive upsurge in requests to learn Javascript and show how I work. So I have begun doing workshops at Makerspaces and schools around the world. <br><br>
+
+My next workshop will be held in Singapore on the 7th and 8th of January.
+More info here: <a href="https://www.eventbrite.sg/e/pick-a-word-live-your-code-creative-computing-workshop-for-adults-tickets-30289091488?aff=ehomecard">https://www.eventbrite.sg/e/pick-a-word-live-your-code-creative-computing-workshop-for-adults-tickets-30289091488?aff=ehomecard</a> <br>
+
+
+
+</p>
+
+
+
+
+Creative Direction. Concept, design, programming <br>
+Technologies: PHP, MySql, Flash<br>
+Medium: Online
+
+
+</div>
+
+
+	</div><!-- container -->
+
+
+	<?php include("footer.php"); ?>
+	<?php include("tracker.php"); ?>
+
+
+
+<!-- End Document
+================================================== -->
+</body>
+</html>
